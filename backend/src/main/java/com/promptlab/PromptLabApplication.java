40f915047242f80485d628e5191a2247e4ef7d7d
dulev23 <nikoladulev13@gmail.com>
2026-06-06 20:38:24 +0200

    Initial project setup

---
+++ backend/src/main/java/com/promptlab/PromptLabApplication.java
@@ -0,0 +1,11 @@
+package com.promptlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromptLabApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PromptLabApplication.class, args);
+    }
+}
